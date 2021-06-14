018eaba72060c8ac8314d5c64bec05fd360264cb
Andrew-Romanov <andrewromanov@yahoo.com>
2021-06-14 07:58:10 +0300

    Добавлен функционал открывания и закрывания

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './sass/main.scss';
+
+const openModalBtnEl = document.querySelector('.open-button');
+const closeModalBtnEl = document.querySelector('.close-button');
+const backdropEl = document.querySelector('.backdrop');
+
+openModalBtnEl.addEventListener('click', toggleModal);
+closeModalBtnEl.addEventListener('click', toggleModal);
+backdropEl.addEventListener('click', (event) => {
+  if (event.target === event.currentTarget) toggleModal();
+});
+
+function toggleModal() {
+  document.body.classList.toggle('modal-open');
+  backdropEl.classList.toggle('is-hidden');
+};
