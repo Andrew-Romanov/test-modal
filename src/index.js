1c501bc24627efbabf5ad5f1f36aaaaf3fc32ac8
Andrew-Romanov <andrewromanov@yahoo.com>
2021-06-14 15:38:48 +0300

    Заполнение информацией сделано на Handlebars

--- src/index.js
+++ src/index.js
@@ -1,16 +1,37 @@
 import './sass/main.scss';
 
+import modalMarkup from './templates/modal';
+
 const openModalBtnEl = document.querySelector('.open-button');
 const closeModalBtnEl = document.querySelector('.close-button');
 const backdropEl = document.querySelector('.backdrop');
+const modalInfoEl = document.querySelector('.modal__main-container');
 
-openModalBtnEl.addEventListener('click', toggleModal);
-closeModalBtnEl.addEventListener('click', toggleModal);
+openModalBtnEl.addEventListener('click', openModal);
+closeModalBtnEl.addEventListener('click', closeModal);
 backdropEl.addEventListener('click', (event) => {
-  if (event.target === event.currentTarget) toggleModal();
+  if (event.target === event.currentTarget) closeModal();
 });
+
+function openModal() {
+  renderModalInfo();
+  toggleModal();
+};
+
+function closeModal() {
+  toggleModal();
+  clearModalInfo();
+};
 
 function toggleModal() {
   document.body.classList.toggle('modal-open');
   backdropEl.classList.toggle('is-hidden');
 };
+
+function renderModalInfo() {
+  modalInfoEl.insertAdjacentHTML('beforeend', modalMarkup());
+};
+
+function clearModalInfo() {
+  modalInfoEl.innerHTML = '';
+}
